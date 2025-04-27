c6c21804e259353877b48453f8f9de6c84fe1178
Arajlym Kabykenova <arailym.kabykenova@icloud.com>
2025-04-27 13:20:23 +0500

    remove from settings os

--- server/server/settings.py
+++ server/server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 
 
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
